afee400357b34cdab85bb3d9dcc459438112aba7
sphoujdar <sm.phoujdar@gmail.com>
2020-11-10 11:34:34 +0530

    Modified : Now includes last name logic too.

--- patternMatching.sh
+++ patternMatching.sh
@@ -4,13 +4,13 @@ echo "Welcome to User Registeration Program"
 
 
 #Patterns to match
-user_name_pattern="^[A-Z].{2,}$"
+user_name_pattern="^[A-Z].{2,}\s[A-Z].{2,}$"
 
 
 
 string_accepted_flag=0
 while [ $string_accepted_flag -eq 0 ] ; do
-	read -p "Enter User Name - ( Start with a cap and minimum 3 characters ) : " name
+	read -p "Enter First and Last Name separated by a space- (Both start with a cap and minimum 3 characters ) : " name
 	if [[ $name =~ $user_name_pattern ]] ; then
 		string_accepted_flag=1
 		echo \[ $name \] is accepted as User Name
