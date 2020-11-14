9f42c9668cd940feaab1b1d50d581200321b4d56
sphoujdar <sm.phoujdar@gmail.com>
2020-11-14 16:46:32 +0530

    Add : Email pattern matching logic added , not working for all logics, especially negation logic.

--- patternMatching.sh
+++ patternMatching.sh
@@ -1,19 +1,44 @@
-#!/bin/bash -x
+#!/bin/bash
+
+shopt -s extglob
 
 echo "Welcome to User Registeration Program"
 
 
 #Patterns to match
 user_name_pattern="^[A-Z].{2,}\s[A-Z].{2,}$"
+email_pattern="^[\.\+-_a-zA-Z0-9]+@([a-zA-Z0-9]{1,}\.[a-z]{3}){1}?(\.[a-z]{2,3}){0,1}$"
+
+
+declare -a names_check=("Aada Foop" "shubham phoujdar")
+declare -a emails_check=( "emailabc@yahoo.com" "abc-100@yahoo.com" "abc.100@yahoo.com" "abc111@abc.com" "abc-100@abc.net" "abc.100@abc.com.au" "abc@1.com" "abc@gmail.com.com" "abc+100@gmail.com" "-------------------------------------------------------------------------------" "abc" "abc@.com.my" "abc123@gmail.a" "abc123@.com" "abc123@.com.com" ".abc@abc.com" "abc()*@gmail.com" "abc@%*.com" "abc..2002@gmail.com" "abc.@gmail.com" "abc@abc@gmail.com" "abc@gmail.com.1a" "abc@gmail.com.aa.au" )
 
 
 
-string_accepted_flag=0
-while [ $string_accepted_flag -eq 0 ] ; do
-	read -p "Enter First and Last Name separated by a space- (Both start with a cap and minimum 3 characters ) : " name
+echo ----------------------NAME CHECK-------------------------
+for name in "${names_check[@]}" ; do
 	if [[ $name =~ $user_name_pattern ]] ; then
-		string_accepted_flag=1
-		echo \[ $name \] is accepted as User Name
-	else echo The User Name is Invalid , Please Enter a Valid User Name
+		echo \[ $name \] accepted
+	else 
+		echo \[ $name \] rejected
 	fi
 done
+echo ---------------------EMAIL CHECK--------------------------
+	for email in "${emails_check[@]}" ; do
+	        if [[ $email =~ $email_pattern ]] ; then
+	                echo \[ $email \] accepted
+	        else echo \[ $email \] rejected
+	        fi
+	done
+#echo --------------------- CHECK--------------------------
+#        for email in "${emails_check[@]}" ; do
+#                if [[ $email =~ $email_pattern ]] ; then
+#                        echo \[ $email \] accepted
+#                else echo \[ $email \] rejected
+#                fi
+#        done
+
+
+
+
+
