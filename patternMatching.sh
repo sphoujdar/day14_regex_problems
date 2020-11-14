812c9d6b26c7e9566bbe386158a565206e0b991b
sphoujdar <sm.phoujdar@gmail.com>
2020-11-14 17:37:41 +0530

    Added : Password Rule - min 8 characters in password

--- patternMatching.sh
+++ patternMatching.sh
@@ -9,11 +9,14 @@ echo "Welcome to User Registeration Program"
 user_name_pattern="^[A-Z].{2,}\s[A-Z].{2,}$"
 email_pattern="^[\.\+-_a-zA-Z0-9]+@([a-zA-Z0-9]{1,}\.[a-z]{3}){1}?(\.[a-z]{2,3}){0,1}$"
 number_pattern="^[0-9]{2}\s[0-9]{10}$"
+password_pattern="^.{8,}$"
 
 
 declare -a names_check=("Aada Foop" "----------------------------------------------" "shubham phoujdar")
 declare -a emails_check=( "emailabc@yahoo.com" "abc-100@yahoo.com" "abc.100@yahoo.com" "abc111@abc.com" "abc-100@abc.net" "abc.100@abc.com.au" "abc@1.com" "abc@gmail.com.com" "abc+100@gmail.com" "------------------------------------------------" "abc" "abc@.com.my" "abc123@gmail.a" "abc123@.com" "abc123@.com.com" ".abc@abc.com" "abc()*@gmail.com" "abc@%*.com" "abc..2002@gmail.com" "abc.@gmail.com" "abc@abc@gmail.com" "abc@gmail.com.1a" "abc@gmail.com.aa.au" )
 declare -a numbers_check=( "91 8806615534" "---------------------------------------------------" "020 2544 2920" "8558545213" "91 6544824")
+declare -a passwords_check=("1236E547989" "duaw#dvgADws1" "----------------------------------------------" "shubham phoujdar" "dsawdg##ht" "184556" "adwdf")
+
 
 
 echo ----------------------NAME CHECK-------------------------
@@ -38,6 +41,16 @@ echo -------------------NUMBERS CHECK------------------------
                 else echo \[ $number \] rejected
                 fi
         done
+
+echo ------------------PASSWORD CHECK-----------------------
+        for password in "${passwords_check[@]}" ; do
+                if [[ $password =~ $password_pattern ]] ; then
+                        echo \[ $password \] accepted
+                else echo \[ $password \] rejected
+                fi
+        done
+
+
 
 
 
